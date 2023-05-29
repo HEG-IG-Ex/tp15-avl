885e30fab96f8d1d46a88c7684fda00007303752
Victor Hueni <victor.hueni@protonmail.com>
2023-05-29 18:23:14 +0200

    add some explanation comments

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,29 @@ public class Main {
     // SRC : AVL : https://www.youtube.com/watch?v=Jj9Mit24CWk
     // SRC : Print2D : https://www.geeksforgeeks.org/print-binary-tree-2-dimensions/
 
+/*
+    Terms
+    =======
+    Level of a node is the number of edges between root node and node in question
+    Height is the level of between the node in question and the further null node of the tree
+
+    Balance
+    =======
+    a tree or sub tree is considered balanced when : balance(root) E [-1, 1]
+    balance(node) = height(leftChild) - height(rightChild)
+
+    negative balances means left rotation
+    positive balances means right rotation
+
+    Rotation scenario
+    =================
+    leftleft situation => balance > 1 => solved using a single right rotation
+    rightright situation  =>balance < -1 => solved using a singler left rotation
+    leftright sitation => balances needs to have the same sign as it's parent => apply a left roation then a right
+    rightleft situation => balances needs to have the same sign as it's parent =>  apply a right rotation then a left
+
+ */
+
     public static void main(String[] args) {
 	// write your code here
         List<Integer> list = new ArrayList<>();
